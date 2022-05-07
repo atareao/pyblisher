14f7317f163975d54732b386bd22e5e2ff740910
Lorenzo Carbonell <lorenzo.carbonell.cerezo@gmail.com>
2022-05-07 06:27:44 +0200

    use retries

--- src/main.py
+++ src/main.py
@@ -23,6 +23,8 @@
 
 
 import os
+import sys
+import logging
 import requests
 import yt_dlp
 from dotenv import load_dotenv
@@ -36,10 +38,19 @@ from plumbum import local
 from requests_oauthlib import OAuth1
 from aupload import VideoTweet
 from mastodonapi import MastodonClient
+from fdapi import PeerTube
+from retry import retry
 
-
+LOG_FORMAT = "%(levelname)s %(asctime)s - %(message)s"
 Table.DATABASE = "/app/database.db"
 Log.LEVEL = Log.DEBUG
+
+logging.basicConfig(stream=sys.stdout,
+                    filemode="w",
+                    format=LOG_FORMAT,
+                    level=logging.INFO)
+
+logger = logging.getLogger()
 
 load_dotenv()
 Video.inicializate()
@@ -61,13 +72,13 @@ async def update():
         yt_videos = youtube.get_videos(yt_channel, db_video.published_at)
         for yt_video in yt_videos:
             if yt_video['yt_id'] != db_video.yt_id:
-                print("Sin publicar")
+                logger.info("Sin publicar")
                 athread = Thread(target=populate, args=(yt_video,))
                 athread.daemon = True
                 athread.start()
                 break
             else:
-                print("Publicado")
+                logger.info("Publicado")
     else:
         yt_videos = youtube.get_videos(yt_channel)
         for yt_video in yt_videos:
@@ -89,55 +100,84 @@ def populate(yt_video):
     origen = "/tmp/origen.mp4"
     destino = "/tmp/destino.mp4"
     clean(origen, destino)
-    tries = 3
-    while tries > 0:
-        try:
-            yt_id = yt_video["yt_id"]
-            title = yt_video['title']
-            link = yt_video['link']
-            message = title + '\n\n'
-            largo = 270 - len(message) - len(link)
-            message += yt_video['description'][0:largo] + '...\n\n' + link
-            message_discord = "**" + title + "**\n"
-            message_discord += yt_video["description"] + "\n" + link
-            url = f"https://www.youtube.com/watch?v={yt_id}"
-            ydl_opts = {"outtmpl": "/tmp/origen",
-                        "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
-                        "retries": 5}
-            print("Start download")
-            with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-                ydl.download([url])
-            print("End download")
-            convert(origen, destino)
-            tweet(message, destino)
-            print("Start save YouTube video")
-            Video.new(yt_video['title'],
-                      yt_video['description'],
-                      yt_video['yt_id'],
-                      yt_video['link'],
-                      yt_video['published_at'])
-            print("End save YouTube video")
-            tries = 0
-            telegramea(message, destino)
-            discordea(message_discord)
-            toot(message, destino)
-        except Exception as exception:
-            print(exception)
-            print(f"Can not download. Try number: {tries}")
-            tries -= 1
+    yt_id = yt_video["yt_id"]
+    title = yt_video['title']
+    description = yt_video["description"]
+    link = yt_video['link']
+    message = title + '\n\n'
+    largo = 270 - len(message) - len(link)
+    message += yt_video['description'][0:largo] + '...\n\n' + link
+    message_discord = "**" + title + "**\n"
+    message_discord += yt_video["description"] + "\n" + link
+    message_mastodon = f"{title}\n\n{description}"
+    max_length = 495 - len(link)
+    if len(message_mastodon) > max_length:
+        message_mastodon = message_mastodon[:max_length]
+    message_mastodon = f"{message_mastodon}\n\n{link}"
+
+    try:
+        download(yt_id)
+    except Exception as exception:
+        logger.error(exception)
+        logger.info("Can not continue")
+        return
+    try:
+        convert(origen, destino)
+        tweet(message, destino)
+        logger.info("Start save YouTube video")
+        Video.new(yt_video['title'],
+                  yt_video['description'],
+                  yt_video['yt_id'],
+                  yt_video['link'],
+                  yt_video['published_at'])
+        logger.info("End save YouTube video")
+    except Exception as exception:
+        logger.error(exception)
+        logger.info("Can not continue")
+        return
+    try:
+        telegramea(message, destino)
+    except Exception as exception:
+        logger.error(exception)
+    try:
+        discordea(message_discord)
+    except Exception as exception:
+        logger.error(exception)
+    try:
+        toot(message_mastodon, destino)
+    except Exception as exception:
+        logger.error(exception)
+    try:
+        export2PeerTube(title, description, origen)
+    except Exception as exception:
+        logger.error(exception)
     clean(origen, destino)
 
 
+@retry(tries=3, delay=30, logger=logger)
+def download(yt_id):
+    logger.info("Start download")
+    url = f"https://www.youtube.com/watch?v={yt_id}"
+    ydl_opts = {"outtmpl": "/tmp/origen",
+                "format": "bestvideo[ext=mp4]+bestaudio[ext=m4a]/mp4",
+                "retries": 5}
+    logger.info("Start download")
+    with yt_dlp.YoutubeDL(ydl_opts) as ydl:
+        ydl.download([url])
+    logger.info("End download")
+
+
 def convert(origen, destino):
-    print("Start cutting")
+    logger.info("Start cutting")
     ffmpeg = local['ffmpeg']
     ffmpeg["-y", "-ss", "0", "-to", "45", "-i", origen, "-c", "copy",
            destino]()
-    print("End cutting")
+    logger.info("End cutting")
 
 
+@retry(tries=3, delay=10, logger=logger)
 def tweet(message, filename):
-    print("Start tweeting")
+    logger.info("Start tweeting")
     consumer_key = os.getenv("TW_CONSUMER_KEY")
     consumer_secret = os.getenv("TW_CONSUMER_SECRET")
     access_token = os.getenv("TW_ACCESS_TOKEN")
@@ -150,37 +190,51 @@ def tweet(message, filename):
     video_tweet.upload_append()
     video_tweet.upload_finalize()
     video_tweet.tweet()
-    print("End tweeting")
+    logger.info("End tweeting")
 
 
+@retry(tries=3, delay=5, logger=logger)
 def toot(message, filename):
-    print("Start message in Mastodon")
+    logger.info("Start message in Mastodon")
     base_uri = os.getenv("MASTODON_BASE_URI")
     access_token = os.getenv("MASTODON_ACCESS_TOKEN")
     mastodon_client = MastodonClient(base_uri, access_token)
     mastodon_client.toot_with_media(message, filename)
-    print("End message in Mastodon")
+    logger.info("End message in Mastodon")
 
 
+@retry(tries=3, delay=5, logger=logger)
 def discordea(message):
-    print("Start message in Discord")
+    logger.info("Start message in Discord")
     channel = os.getenv("DISCORD_CHANNEL")
     token = os.getenv("DISCORD_TOKEN")
     url = f"https://discord.com/api/webhooks/{channel}/{token}"
     data = {"content": message}
     requests.post(url, data)
-    print("End message in Discord")
+    logger.info("End message in Discord")
 
 
+@retry(tries=3, delay=5, logger=logger)
 def telegramea(message, filename):
-    print("Start message in Telegram")
+    logger.info("Start message in Telegram")
     chat_id = os.getenv("TELEGRAM_CHANNEL")
     token = os.getenv("TELEGRAM_TOKEN")
     data = {"chat_id": chat_id, "caption": message}
     url = f"https://api.telegram.org/bot{token}/sendVideo"
     with open(filename, "rb") as fr:
         requests.post(url, data=data, files={"video": fr})
-    print("End message in Telegram")
+    logger.info("End message in Telegram")
+
+
+@retry(tries=3, delay=60, logger=logger)
+def export2PeerTube(title, description, filename):
+    logger.info("Start export to PeerTube")
+    pt_path = os.getenv("PT_PATH")
+    channel_id = os.getenv('PT_CHANNEL_ID')
+    peerTube = PeerTube(pt_path)
+    response = peerTube.upload(channel_id, filename, title, description)
+    logger.info(response)
+    logger.info("End export to PeerTube")
 
 
 def clean(origen, destino):
